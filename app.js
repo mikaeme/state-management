9906cd78e84b27c0ba30435b4e1aa1d134a44e9b
Nakkikone <nakkikone@Nakkikones-MacBook-Pro.local>
2019-11-21 10:33:03 +0200

    password

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 'use strict';
 const express = require('express');
 const session = require('express-session');
+const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
+
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
@@ -15,6 +24,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -36,22 +48,20 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 
-app.get('/secret', (req, res) => {
-  if (req.session.logged) {
-    res.render('secret');
-  } else {
-    res.redirect('/form');
-  }
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 
-app.post('/login', (req, res) => {
-  if (username === req.body.username && password === req.body.password) {
-    req.session.logged = true;
-    res.redirect('/secret');
-  } else {
-    req.session.logged = false;
-    res.redirect('/form');
-  }
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
+
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
+    });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
